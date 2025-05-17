b69478eb871f8b68b3b86c2479c57a011c19c35a
Hao Nghiem <qtvhao@gmail.com>
2025-05-17 11:44:24 +0700

    Add

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { AppBootstrapper } from "./bootstrap/AppBootstrapper";
+import { Application } from "./Infrastructure/Kernel/Application";
 
-export { AppBootstrapper };
+export { AppBootstrapper, Application };
