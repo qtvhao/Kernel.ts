c355f72b659471cdad933a809be7b3b4f738b48a
Hao Nghiem <qtvhao@gmail.com>
2025-05-19 00:51:18 +0700

    Add

---
+++ src/Application/Bus/Command/CommandBus.ts
@@ -0,0 +1,22 @@
+// Kernel.ts/src/Application/Bus/Command/CommandBus.ts
+
+import { ICommand, ICommandHandlerResolver } from "contracts.ts";
+
+export class CommandBus {
+    constructor(private readonly handlerResolver: ICommandHandlerResolver) {}
+
+    async dispatch<
+        TCommand extends ICommand,
+        TResult extends void | object = void,
+    >(command: TCommand): Promise<TResult> {
+        const handler = this.handlerResolver.resolve<TCommand, TResult>(
+            command,
+        );
+        if (!handler) {
+            throw new Error(
+                `No handler found for command: ${command.constructor.name}`,
+            );
+        }
+        return handler.execute(command);
+    }
+}
