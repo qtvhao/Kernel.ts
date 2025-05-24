0fe1a6a01f68615b1b67326843ea78a18164b11c
Hao Nghiem <qtvhao@gmail.com>
2025-05-25 00:26:51 +0700

    Add

--- src/Infrastructure/bootstrap/ConfigurationService.ts
+++ src/Infrastructure/bootstrap/ConfigurationService.ts
@@ -4,4 +4,26 @@ export class ConfigurationService implements IConfigurationService {
     getEventBusDriver(): string {
         return process.env.EVENT_BUS_DRIVER || "inmemory";
     }
+
+    getKafkaBrokers(): string[] {
+        this.ensureKafkaVar("KAFKA_BROKERS");
+        const brokers = process.env.KAFKA_BROKERS;
+        return brokers ? brokers.split(",") : [];
+    }
+
+    getKafkaClientId(): string {
+        this.ensureKafkaVar("KAFKA_CLIENT_ID");
+        return process.env.KAFKA_CLIENT_ID || "";
+    }
+
+    getKafkaGroupId(): string {
+        this.ensureKafkaVar("KAFKA_GROUP_ID");
+        return process.env.KAFKA_GROUP_ID || "";
+    }
+
+    private ensureKafkaVar(envVar: string): void {
+        if (this.getEventBusDriver() === "kafka" && !process.env[envVar]) {
+            throw new Error(`${envVar} must be set for Kafka driver`);
+        }
+    }
 }
